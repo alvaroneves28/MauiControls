5cd81d4cc454c2917406b5466d1078ab96da0482
Álvaro Neves <alvaro.neves2019@gmail.com>
2025-06-04 14:59:27 +0100

    Add project files.

---
+++ MauiControls/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MauiControls.Pages;
+
+namespace MauiControls
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var navPage = new NavigationPage(new MauiPresentationControls());
+
+            return new Window(navPage);
+        }
+    }
+}
