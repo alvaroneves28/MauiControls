9ef2311d4dc5920fb0ac3fae0430d56a05d26508
Álvaro Neves <alvaro.neves2019@gmail.com>
2025-06-05 16:42:02 +0100

    Editing and activity controls

--- MauiControls/App.xaml.cs
+++ MauiControls/App.xaml.cs
@@ -11,7 +11,7 @@ namespace MauiControls
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            var navPage = new NavigationPage(new MauiSetValueControls());
+            var navPage = new NavigationPage(new MauiEditControls());
 
             return new Window(navPage);
         }
